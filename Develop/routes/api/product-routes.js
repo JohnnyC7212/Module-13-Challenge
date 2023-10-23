e05370701426810ffaede9a89ad1e04c7cdd295b
JohnnyC7212 <jonathan.mont7212.com>
2023-10-22 20:36:52 -0400

    worked on product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,20 +3,55 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+// Assuming you have imported your models and set up your router and Sequelize appropriately
+
+// Get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: Tag },
+      ],
+    });
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await product.findByPk(req.params.id, {
+      include: [
+                { model: Category},
+                { model: Tag} 
+               ],
+    });
+    
+    if (product) {
+      res.status(404).json({ message: 'No product found!'})
+      return;
+    }
+    res.status(200).json(product);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const newProduct = await newProduct.create({
+      reader_id: req.body.reader_id,
+    });
+    res.status(200).json(newProduct);
+   }catch (err) {
+    res.status(400).json(err);
+   }
   /* req.body should look like this...
     {
       product_name: "Basketball",
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,11 +2,25 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
+// Assuming you have imported your models and set up your router and Sequelize appropriately
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Get all tags, including associated Product data
+router.get('/', async (req, res) => {
+  try {
+    // Find all tags 
+    const tags = await Tag.findAll({
+      include: [
+        { model: Product }, // Include associated Product data
+      ],
+    });
+
+    res.status(200).json(tags);
+  } catch (err) {
+  
+    res.status(500).json(err);
+  }
 });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
